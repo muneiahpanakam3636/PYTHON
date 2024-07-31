eb1f828e57498b98b507da8d27af401c408fc47b
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-07-31 11:24:45 +0530

    python

---
+++ DATATYPES/LTSD/one.py
@@ -0,0 +1,13 @@
+l1 = [10,20,30,40,50,60,70]
+t1 =(10,20,30,40,60,50,70)
+s1 = {10,20,30,40,50,60,70}
+emp1 = {'id':100,'name':'rahul'}
+
+print(l1)
+print(type(l1))
+print(t1)
+print(type(t1))
+print(s1)
+print(type(s1))
+print(emp1)
+print(type(emp1))
