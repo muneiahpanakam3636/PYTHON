3e17325887b080fbe16a4cb76668fa77794eae62
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-08-05 13:35:09 +0530

    python

---
+++ operators/sequenceorcollection.py
@@ -0,0 +1,14 @@
+# what is sequence/collection?
+#list,string,set,tuple,dict
+
+l1 = [10,20,30,40,50]
+t1 = (10,20,30,40,50)
+s1 ={10,20,30,40,50}
+str1 = 'raani'
+dict ={'id':101,'name':'radha'}
+
+print(10 in l1)
+print(20 in t1)
+print(30 in s1)
+print('a' in str1)
+print('name' in dict)
