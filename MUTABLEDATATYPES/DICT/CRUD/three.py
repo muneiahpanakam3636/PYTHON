06798c66091e07f6d11a18c6ee00ea6f7f590e39
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-08-29 11:25:12 +0530

    python

---
+++ MUTABLEDATATYPES/DICT/CRUD/three.py
@@ -0,0 +1,12 @@
+#  what is dict object?  group of key:values are one entity.
+#or 
+# group of properties (property name and value)
+
+emp = {
+    'eid ':101,
+    'ename':"Rahul",
+    'esal':45000.00,
+    'avila':True
+}
+print(emp)
+#{'eid ': 101, 'ename': 'Rahul', 'esal': 45000.0, 'avila': True}
