bec1792faf6d590a0f4ece02d3203bcd17b892bf
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-09-03 12:18:43 +0530

    python

---
+++ FUNCTIONS/inner functions/lambda.py
@@ -0,0 +1,15 @@
+#  nameless function is called as LAMBDA(lambda).
+
+# def add (x):
+#     return x+1
+# r1 = add(100)
+# print(r1)
+#101
+
+#  it will written as below
+
+add = lambda x:x+1
+r1 =add(100)
+print(r1)
+
+#101
